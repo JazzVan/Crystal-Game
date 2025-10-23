8c200e77d55dae98d87f059b1521abab4e468c73
jnichol8 <j_nicholas@u.pacific.edu>
2025-10-22 19:41:29 -0600

    first commit

---
+++ Assets/Scripts/TileRemover.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileRemover : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mouseWorldPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+            // Raycast against all 2D colliders at this point
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mouseWorldPos, Vector2.zero);
+
+            if (hits.Length > 0)
+            {
+                Tilemap topTilemap = null;
+                int highestOrder = int.MinValue;
+
+                foreach (RaycastHit2D hit in hits)
+                {
+                    Tilemap tilemap = hit.collider.GetComponent<Tilemap>();
+                    if (tilemap != null)
+                    {
+                        var renderer = tilemap.GetComponent<TilemapRenderer>();
+                        if (renderer != null && renderer.sortingOrder > highestOrder)
+                        {
+                            highestOrder = renderer.sortingOrder;
+                            topTilemap = tilemap;
+                        }
+                    }
+                }
+
+                // Remove tile only from the top-most tilemap
+                if (topTilemap != null)
+                {
+                    Vector3Int cellPos = topTilemap.WorldToCell(mouseWorldPos);
+                    if (topTilemap.HasTile(cellPos))
+                    {
+                        topTilemap.SetTile(cellPos, null);
+                    }
+                }
+            }
+        }
+    }
+}
+
